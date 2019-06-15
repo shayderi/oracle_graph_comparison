0693d518763a57951fa7bf422c8c0bfc51220524
shay.deri <shay.deri@gmail.com>
2019-06-15 22:58:24 +0300

    config

--- expansionProvider.js
+++ expansionProvider.js
@@ -1,9 +1,39 @@
+const request = require('request')
 const sampleResults = require('./sample1')
+const config = require('./config')
 
 const sample = () => sampleResults
 
-const fetchExpand = (typeValueArr) => {
-  
+async function fetchExpand(typeValueArr) {
+  const requestBody = {
+
+  }
+
+  const graphHeaders = {}
+  const oracleHeaders = {}
+
+  const graphOptions = {
+    url: config.graphUrl,
+    method: 'post',
+    json: true,
+    body: requestBody,
+    graphHeaders
+  };
+
+  const oracleOptions = {
+    url: config.oracleUrl,
+    method: 'post',
+    json: true,
+    body: requestBody,
+    oracleHeaders
+  };
+
+  const promiseArr = [
+    request(graphOptions),
+    request(oracleOptions)
+  ]
+  const results = await Promise.all(promiseArr);
+  return results
 }
 
 module.exports = {
